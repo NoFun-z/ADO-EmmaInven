b71e0c2d517d220163d9870b79734f43d69b0bae
NoFun <hpham15@ncstudents.niagaracollege.ca>
2023-11-22 02:05:09 -0500

    ProjectSetUp
    AddedProject

---
+++ Project/FinalProject1210/FinalProject1210/EmmasDataSet.cs
@@ -0,0 +1,22 @@
+﻿namespace FinalProject1210
+{
+
+
+    partial class EmmasDataSet
+    {
+    }
+}
+
+namespace FinalProject1210.EmmasDataSetTableAdapters
+{
+	partial class CustomerOrderTableAdapter
+	{
+	}
+
+	partial class customerTableAdapter
+    {
+    }
+
+    public partial class employeeFullTableAdapter {
+    }
+}
